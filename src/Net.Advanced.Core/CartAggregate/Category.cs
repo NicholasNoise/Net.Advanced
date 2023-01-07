f80908cf6f9ba36ceb465f5d4bc5f6c768ad75e3
Nikolai Smirnov1 <Nikolai_Smirnov1@epam.com>
2023-01-07 18:55:37 +0400

    Catalog

---
+++ src/Net.Advanced.Core/CartAggregate/Category.cs
@@ -0,0 +1,9 @@
+﻿using Net.Advanced.SharedKernel;
+
+namespace Net.Advanced.Core.CartAggregate;
+public class Category : EntityBase
+{
+  public string Name { get; set; } = string.Empty;
+  public string Image { get; set; } = string.Empty;
+  public int Parent { get; set; }
+}
---
+++ src/Net.Advanced.Core/CartAggregate/Product.cs
@@ -0,0 +1,12 @@
+﻿using Net.Advanced.SharedKernel;
+
+namespace Net.Advanced.Core.CartAggregate;
+public class Product : EntityBase
+{
+  public string Name { get; set; } = string.Empty;
+  public string Description { get; set; } = string.Empty;
+  public string Image { get; set; } = string.Empty;
+  public int CategoryId { get; set; }
+  public decimal Price { get; set; }
+  public uint Amount { get; set; }
+}
